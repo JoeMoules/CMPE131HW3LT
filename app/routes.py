0419a974362e99511d37825ec9c73f0253030e21
Joe Moules <itsjoemoules@gmail.com>
2022-03-28 12:25:29 -0700

    revisions

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
-from app import myapp_obj
+from app import myobj
 from flask import flash, render_template
 
 name="Filler"
 city_names=["Hanford", "Hanford 2: Electric Boogaloo"]
-@myapp_obj.route("/")
+@myobj.route("/")
 
 def home():
     return render_template('home.html',name=name, city_names=city_names)

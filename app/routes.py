f4cff07a42d051275e1c556fd70cd1b2cd702ab4
Joe Moules <itsjoemoules@gmail.com>
2022-03-28 19:51:30 -0700

    reverting some changes

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,9 @@
 from app import myobj
 from flask import flash, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
 
 name="Filler"
 city_names=["Hanford", "Hanford 2: Electric Boogaloo"]
-@myobj.route("/", methods=['GET', 'POST'])
+@myobj.route("/")
 
 def home():
-    form=input()
-    if form.validate_on_submit():
-        flash('{}' .format(input.cityName.data))
-    return render_template('home.html',name=name, city_names=city_names, form=form)
+    return render_template('home.html',name=name, city_names=city_names)
